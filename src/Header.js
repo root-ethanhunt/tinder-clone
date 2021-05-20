60dcdd15505f663e8c34ef61cbc6c412c20f1665
VIKASH KUMAR <vikash.hunt111@gmail.com>
2021-05-20 22:20:01 +0530

    react-tinder-clone

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+
+function Header() {
+  return (
+    <div className="header"> 
+    <IconButton>
+    <PersonIcon fontSize="large" className="header_icon" />
+    </IconButton>
+
+    <img className="header_logo" src="https://image000.flaticon.com/png/512/408/408790.png" alt=""/>
+
+<IconButton>
+<ForumIcon fontSize="large" className="header_icon"/>
+</IconButton>
+   
+    </div>
+
+
+  );
+}
+
+export default Header;
